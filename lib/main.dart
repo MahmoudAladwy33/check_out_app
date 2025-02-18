f0b37c449accf21ca82491c2c2de018e3e9a3385
MahmoudAladwy <mahmoudaladwy774@gmail.com>
2025-02-18 17:49:14 +0200

    - create my cart view

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:check_out_app/features/check_out/ui/my_cart_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,6 +12,7 @@ class CheckoutApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: MyCartView(),
     );
   }
 }
