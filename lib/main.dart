85fc5f6568ae254141f97da38c88960bdb250cdc
MahmoudAladwy <mahmoudaladwy774@gmail.com>
2025-02-18 17:34:05 +0200

    - first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CheckoutApp());
+}
+
+class CheckoutApp extends StatelessWidget {
+  const CheckoutApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
